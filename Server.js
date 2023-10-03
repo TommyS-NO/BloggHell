359035b58d35dcbefb3ea0855f2e7685ca41f99a
Fizboo <tommy.skorge@hotmail.com>
2023-10-03 11:57:27 +0200

    Admin Dashboard added, login works

--- Server.js
+++ Server.js
@@ -5,29 +5,31 @@ const path = require("path");
 const app = express();
 const PORT = 3234; //postnummer for MORROskyld
 
-const mainRoutes = require("./server/routes/main");
-app.use("/", mainRoutes);
+const session = require("express-session");
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-//Middleware
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-//Sette EJS som visningsmotor istedet for HTML filer.!
+// Sette EJS som visningsmotor istedet for HTML filer.!
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Routes inProgress
+// Routes
+const mainRoutes = require("./server/routes/main");
+app.use("/", mainRoutes);
+app.use("/", require("./server/routes/admin"));
 
-// app.use('/', require('./server/routes/main'));
-// app.use('/', require('./server/routes/admin'));
-
-app.get("/", (req, res) => {
-  res.send("Hello from BloggHell!");
-});
-
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ Server/Routes/admin.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+
+const adminDataPath = path.join(__dirname, "../data/admin.json");
+const postsDataPath = path.join(__dirname, "../data/blogginnlegg.json");
+
+// GET Admin Login Page
+router.get("/admin", (req, res) => {
+  res.render("admin/index", { error: req.session.error });
+  req.session.error = null; // Clear the error after displaying it
+});
+
+// POST Admin Login
+router.post("/admin", (req, res) => {
+  const { username, password } = req.body;
+
+  // Validering
+  const validChars = /^[a-zA-Z0-9æøåÆØÅ]+$/;
+  if (!username.match(validChars) || !password.match(validChars)) {
+    req.session.error = "Ugyldige tegn i brukernavn eller passord.";
+    return res.redirect("/admin");
+  }
+
+  const adminData = JSON.parse(fs.readFileSync(adminDataPath, "utf-8"));
+
+  if (username === adminData.username && password === adminData.password) {
+    req.session.isAuthenticated = true;
+    res.redirect("/dashboard");
+  } else {
+    req.session.error = "Feil brukernavn eller passord.";
+    res.redirect("/admin");
+  }
+});
+
+// GET Admin Dashboard
+router.get("/dashboard", (req, res) => {
+  if (!req.session.isAuthenticated) {
+    return res.redirect("/admin");
+  }
+  const posts = JSON.parse(fs.readFileSync(postsDataPath, "utf-8"));
+  res.render("admin/dashboard", { posts });
+});
+
+// GET Add New Post
+router.get("/add-post", (req, res) => {
+  if (!req.session.isAuthenticated) {
+    return res.redirect("/admin");
+  }
+  res.render("admin/add-post");
+});
+
+// POST Add New Post
+router.post("/add-post", (req, res) => {
+  if (!req.session.isAuthenticated) {
+    return res.redirect("/admin");
+  }
+  const newPost = {
+    id: Date.now(),
+    title: req.body.title,
+    content: req.body.content,
+    dateCreated: new Date().toISOString(),
+  };
+  const posts = JSON.parse(fs.readFileSync(postsDataPath, "utf-8"));
+  posts.push(newPost);
+  fs.writeFileSync(postsDataPath, JSON.stringify(posts, null, 4));
+  res.redirect("/dashboard");
+});
+
+module.exports = router;
