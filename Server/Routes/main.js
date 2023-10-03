53f3f071d42ed593903f3f29c9ca9c62c70d8fdb
Fizboo <tommy.skorge@hotmail.com>
2023-10-03 22:48:51 +0200

    noe css

--- Server/Routes/main.js
+++ Server/Routes/main.js
@@ -1,37 +1,52 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
 const postsDataPath = path.join(__dirname, "../data/blogginnlegg.json");
 
 // GET - Hente alle blogginnlegg
-router.get("/api/posts", (req, res) => {
-  const posts = JSON.parse(fs.readFileSync(postsDataPath, "utf-8"));
-  res.json(posts);
+router.get("/api/posts", async (req, res) => {
+  try {
+    const data = await fs.readFile(postsDataPath, "utf-8");
+    const posts = JSON.parse(data);
+    res.json(posts);
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
 });
 
 // POST - "Like" et innlegg
-router.post("/api/like/:id", (req, res) => {
-  const postId = parseInt(req.params.id);
-  const posts = JSON.parse(fs.readFileSync(postsDataPath, "utf-8"));
-  const post = posts.find((p) => p.id === postId);
-  if (!post) return res.status(404).send("Innlegget ble ikke funnet.");
-  post.likes += 1;
-  fs.writeFileSync(postsDataPath, JSON.stringify(posts, null, 2));
-  res.json({ likes: post.likes });
+router.post("/api/like/:id", async (req, res) => {
+  try {
+    const postId = parseInt(req.params.id);
+    const data = await fs.readFile(postsDataPath, "utf-8");
+    const posts = JSON.parse(data);
+    const post = posts.find((p) => p.id === postId);
+    if (!post) return res.status(404).send("Innlegget ble ikke funnet.");
+    post.likes += 1;
+    await fs.writeFile(postsDataPath, JSON.stringify(posts, null, 2));
+    res.json({ likes: post.likes });
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
 });
 
 // POST - Legge til en kommentar
-router.post("/api/comment/:id", (req, res) => {
-  const postId = parseInt(req.params.id);
-  const comment = req.body.comment;
-  const posts = JSON.parse(fs.readFileSync(postsDataPath, "utf-8"));
-  const post = posts.find((p) => p.id === postId);
-  if (!post) return res.status(404).send("Innlegget ble ikke funnet.");
-  post.comments.push(comment);
-  fs.writeFileSync(postsDataPath, JSON.stringify(posts, null, 2));
-  res.json({ comment: comment });
+router.post("/api/comment/:id", async (req, res) => {
+  try {
+    const postId = parseInt(req.params.id);
+    const comment = req.body.comment;
+    const data = await fs.readFile(postsDataPath, "utf-8");
+    const posts = JSON.parse(data);
+    const post = posts.find((p) => p.id === postId);
+    if (!post) return res.status(404).send("Innlegget ble ikke funnet.");
+    post.comments.push(comment);
+    await fs.writeFile(postsDataPath, JSON.stringify(posts, null, 2));
+    res.json({ comment: comment });
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
 });
 
 // GET - Hovedsiden
