0128a823a90cc7349202ecab7332e1fbec122d20
Fizboo <tommy.skorge@hotmail.com>
2023-10-05 23:39:11 +0200

    startet on the admin site

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const session = require("express-session");
 
 const app = express();
-const PORT = 3234; //postnummer for MORROskyld
+const PORT = 3234;
 
 // Set up session middleware
 app.use(
@@ -14,6 +14,9 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
+    cookie: {
+      maxAge: 3000,
+    },
   })
 );
 
@@ -25,8 +28,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 const mainRoutes = require("./server/routes/main");
+const adminRoutes = require("./server/routes/admin");
+
 app.use("/", mainRoutes);
-app.use("/", require("./server/routes/admin"));
+app.use("/admin", adminRoutes);
+
+// Logout route
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
 
 // Start server
 app.listen(PORT, () => {
--- Server/Routes/admin.js
+++ Server/Routes/admin.js
@@ -15,34 +15,32 @@ async function ensureAuthenticated(req, res, next) {
   }
 }
 
-router.get("/admin", (req, res) => {
+router.get("/", (req, res) => {
   if (req.session.isAuthenticated) {
-    return res.redirect("/admin-dashboard");
+    return res.sendFile(path.join(__dirname, "../../views/admin.html"));
   }
   res.redirect("/");
 });
 
-router.post("/admin", async (req, res) => {
+router.post("/", async (req, res) => {
   const { username, password } = req.body;
-
   const validChars = /^[a-zA-Z0-9æøåÆØÅ]+$/;
   if (!username.match(validChars) || !password.match(validChars)) {
     return res.send(
       '<script>alert("Ugyldige tegn i brukernavn eller passord."); window.location.href = "/";</script>'
     );
   }
-
   if (username === adminUsername && password === adminPassword) {
     req.session.isAuthenticated = true;
-    res.redirect("/admin-dashboard");
+    res.redirect("/admin");
   } else {
     res.send(
-      '<script>alert("Feil brukernavn eller passord."); window.location.href = "/";</script>'
+      '<script>alert("Ugyldig innlogging."); window.location.href = "/";</script>'
     );
   }
 });
 
-router.get("/admin-dashboard", ensureAuthenticated, async (req, res) => {
+router.get("/admin.html", ensureAuthenticated, async (req, res) => {
   try {
     const posts = JSON.parse(await fs.readFile(postsDataPath, "utf-8"));
     res.render("admin/admin", { posts });
@@ -55,18 +53,38 @@ router.get("/add-post", ensureAuthenticated, (req, res) => {
   res.render("admin/add-post");
 });
 
-router.post("/add-post", ensureAuthenticated, async (req, res) => {
+router.post("/api/new-post", ensureAuthenticated, async (req, res) => {
   try {
+    const { title, content } = req.body;
     const newPost = {
       id: Date.now(),
-      title: req.body.title,
-      content: req.body.content,
+      title: title,
+      content: content,
       dateCreated: new Date().toISOString(),
+      views: 0,
+      likes: 0,
     };
     const posts = JSON.parse(await fs.readFile(postsDataPath, "utf-8"));
-    posts.push(newPost);
+    posts.unshift(newPost);
     await fs.writeFile(postsDataPath, JSON.stringify(posts, null, 4));
-    res.redirect("/admin.html");
+    res.json(newPost);
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
+
+// Ny rute for å slette et innlegg
+router.delete("/api/delete-post/:id", ensureAuthenticated, async (req, res) => {
+  try {
+    const postId = parseInt(req.params.id);
+    const posts = JSON.parse(await fs.readFile(postsDataPath, "utf-8"));
+    const postIndex = posts.findIndex((post) => post.id === postId);
+    if (postIndex === -1) {
+      return res.status(404).send("Innlegget ble ikke funnet.");
+    }
+    posts.splice(postIndex, 1);
+    await fs.writeFile(postsDataPath, JSON.stringify(posts, null, 4));
+    res.json({ success: true });
   } catch (error) {
     res.status(500).send("Server error");
   }
